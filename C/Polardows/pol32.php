7bf48631c944798ea581173f70d5d204f712cd64
otavioguartieri <otaviorg10@gmail.com>
2023-11-16 05:15:30 -0300

    Websys common files

---
+++ C/Polardows/pol32.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pol 11</title>
+    <link rel="stylesheet" href="C/Polardows/style.css">
+    <link rel="shortcut icon" href="C/Polardows/icon.png" type="image/x-icon">
+</head>
+<body>
+    <div class="screen">
+        <div class="desktop">desktop</div>
+        <div class="taskbar">taskbar</div>
+    </div>
+</body>
+</html>
